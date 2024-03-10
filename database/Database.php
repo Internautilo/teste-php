fa2e50b1246139909ab253b1d2524d1021710526
Internautilo <yuribertulucci2004@gmail.com>
2024-03-09 23:23:02 -0300

    added Database abstract class for templating possible database classes

---
+++ database/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database;
+
+use Database\Interfaces\DatabaseRetriever;
+use Database\Interfaces\DatabaseWriter;
+
+abstract class Database implements DatabaseWriter, DatabaseRetriever
+{
+    protected \PDO $pdo;
+
+    //Params for the construct method
+    protected string $dsn;
+    protected string $db_username;
+    protected string $db_password;
+
+    //Writer Abstract Functions
+    abstract public function store(array $data, string $intoTable): int;
+    abstract public function update(array $data, array $where, string $table): int;
+    abstract public function delete(string $table, array $where): int;
+
+    //Retriever Abstract Functions
+    abstract public function fetchAll(string $table): array;
+    abstract public function findOne(string $table, array $where): array;
+}
