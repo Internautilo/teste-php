4eec876113ab6b02e218d786cae7d360876aba36
Internautilo <yuribertulucci2004@gmail.com>
2024-03-12 21:14:15 -0300

    feat: created checkFillables method in UserController

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -11,4 +11,14 @@ class UserController extends UserModel
     function __construct(UserModel $model, MySQL $connection)
     {
     }
+
+    private function checkFillables(array $data): bool
+    {
+        foreach ($data as $dataKeys => $dataValues){
+            if (!in_array($dataKeys, $this->fillable)) {
+                return false;
+            }
+        }
+        return throw new \Exception("Error: One or more field does not exist in the database");
+    }
 }
